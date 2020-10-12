86ae92b14ce2f978ab6f5610c12d5daffdab9c93
Leah Mattern <ladyleahmattern@gmail.com>
2020-10-11 22:21:55 -0400

    Still fixing

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 // Dependencies
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+const piesController = require('./controllers/pies.js');
+
+// Variables
+const app = express();
 const db = mongoose.connection;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/PaleoPie';
 
 // Require Method Override
 const methodOverride = require('method-override');
 
 // Middleware
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
-app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+app.set('view engine', 'jsx');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.use(methodOverride('_method'));
+app.use('/PaleoPie', piesController);
 
-// Mongoose Connection
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/PaleoPie';
+// Mongo Connection
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
@@ -26,11 +30,12 @@ db.once('open', () => {
     console.log("Hello from Mongo...");
 });
 
-// Controllers
-const piesController = require(`./controllers/pies.js`);
-app.use('/PaleoPie', piesController);
-
 // Listening
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`I'm listening on ${PORT}...`);
 });
